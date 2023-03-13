ad383042cb2906e8a853318cf51041d72d29d670
CryptoKiddd <avaliani1232@gmail.com>
2023-03-13 19:08:30 +0400

    fixed bug in get all tours controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,72 +2,74 @@
 const Tour = require('./../models/tourModel')
 
 
-exports.aliasTopTours=(req,res,next)=>{
-    req.query.limit='5';
-    req.query.sort ='-ratingsAverage,price'
+exports.aliasTopTours = (req, res, next) => {
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price'
     req.query.fields = 'name,price,ratingsAverage,sumamry,difficulty'
     next()
 }
 
 class APIFeatures {
-    constructor(query,queryString){
+    constructor(query, queryString) {
         this.query = query,
         this.queryString = queryString
 
     }
 
     filter(){
-         const queryObj = { ...this.queryString }
-         const excludedFields = ['page', 'sort', 'limit', 'fields']
-         excludedFields.forEach(el => delete queryObj[el])
+        const queryObj = { ...this.queryString }
+        const excludedFields = ['page', 'sort', 'limit', 'fields']
+        excludedFields.forEach(el => delete queryObj[el])
 
-    
-         let queryStr = JSON.stringify(queryObj)
- 
-         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
-         
-         this.query = this.query.find(JSON.parse(queryStr))
-         return this
-        
-        
+
+        let queryStr = JSON.stringify(queryObj)
+
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
+
+        this.query = this.query.find(JSON.parse(queryStr))
+        return this
+
+
 
     }
     sort(){
         if (this.queryString.sort) {
             let sortBy = this.queryString.sort.split(',').join(" ")
-            console.log(sortBy)
+            
             this.query = this.query.sort(sortBy)
 
         } else {
             this.query = this.query.sort('-createdAt')
         }
-      return this
+        return this
     }
+
     limitFields(){
         if (this.queryString.fields) {
             const fields = this.queryString.fields.split(',').join(' ')
             this.query = this.query.select(fields)
         } else {
-            query = query.select('-__v')
+            this.query = this.query.select('-__v')
         }
         return this
     }
+
     paginate(){
         const page = this.queryString.page * 1 || 1;
         const limitBy = this.queryString.limit * 1 || 3
-        const skipBy = (page - 1) * limitBy 
-      
+        const skipBy = (page - 1) * limitBy
+
         this.query = this.query.skip(skipBy).limit(limitBy)
-      
+
         return this
     }
-    
+
 }
 
 
 exports.getAllTours = async (req, res) => {
     try {
-        const features = new APIFeatures(Tour.find(),req.query).filter()
+        const features = new APIFeatures(Tour.find(), req.query).filter().sort().limitFields().paginate()
         const tours = await features.query
 
         res.status(200).json({
