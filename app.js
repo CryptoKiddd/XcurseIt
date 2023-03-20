b1196d88eb164e02a95eabcc85b6fc66f387420d
CryptoKiddd <avaliani1232@gmail.com>
2023-03-20 07:04:36 +0400

    global error handling middleware

--- app.js
+++ app.js
@@ -24,10 +24,21 @@ app.use('/api/v1/users', userRouter)
 
 
 app.all('*',(req,res,next)=>{
-    res.status(404).json({
-        status:'fail',
-        message:`cant find ${req.originalUrl} at the server`
+
+    const err = new Error(`cant find ${req.originalUrl} at the server`)
+    err.statusCode =404
+    err.status = "fail"
+    next(err)
+})
+
+app.use((err,req,res,next)=>{
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "error"
+    res.status(err.statusCode ).json({
+        status:err.status,
+        message:err.message
     })
+    next()
 })
 
 module.exports =app
